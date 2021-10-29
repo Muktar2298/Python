59b0e89c431c8b1890974c3f99fcb1b3ee20527f
Muktar <muktar.webdev@gmail.com>
2021-10-29 12:30:45 +0600

    String formatted & Type function

---
+++ 10.formatted_string&Type_function.py
@@ -0,0 +1,9 @@
+#String formatted (Like => JavaScript "Template String")
+n1 = 10
+n2 = 20
+print(f"{n1} + {n2} = {n1 + n2}")
+
+
+#Type function or show multiple print() in a Line
+print("Nishat Hasan Muktar.", end =" ")
+print("01601091999")
