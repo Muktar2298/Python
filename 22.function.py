70719c30c426f49ac5288bf8cff13e72bb7f3644
Muktar <muktar.webdev@gmail.com>
2021-11-07 22:13:45 +0600

    Function in Python

---
+++ 22.function.py
@@ -0,0 +1,24 @@
+# 1.addition
+def add(a, b):
+    sum = a + b
+    print(sum)
+
+
+add(10, 20)
+add(20, 30)
+
+# 2.without perameter
+def message():
+    print("This is a function without PerameterF")
+
+
+message()
+
+# 3.return something from a function
+def myFunction(a, b):
+    sum = a + b
+    return sum
+
+
+result = myFunction(100, 200)
+print(result)
